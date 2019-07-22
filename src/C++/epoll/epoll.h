c1c7d9e4db6a5ebde52ecbfe234167b9ffa55c87
jim_sun <1543510543@qq.com>
2019-07-22 22:37:09 +0800

    epoll

---
+++ src/C++/epoll/epoll.h
@@ -0,0 +1,27 @@
+#ifdef EPOLL_H
+#define EPOLL_H
+/**
+ * author: jim_sun
+ * date: 2019.07.21 15:02
+*/
+namespace epoll_space {
+    class epoll
+    {
+    private:
+        /* data */
+    public:
+        epoll(/* args */);
+        ~epoll();
+    };
+    
+    epoll::epoll(/* args */)
+    {
+    }
+    
+    epoll::~epoll()
+    {
+    }
+    
+}
+
+#endif
